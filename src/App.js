64d7be2e7e770e8a6e0213f3058ec3986270f72c
Ebrar <16155052@mersin.edu.tr>
2022-07-05 13:16:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from "../src/img/logo.jpeg"
+import {useState,useEffect} from "react"
+import HashLoader from "react-spinners/HashLoader";
+
 
 function App() {
+ 
+  const [loading,setLoading] =useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading?
+        <HashLoader color={"C415AD"} loading={loading}  size={150} />
+        :
+        <div>
+        <img  className='img' src={Logo} alt="" />
+           <h1> MeaDev Channel</h1>
+           <p>Take me out tonight,Because I want to see people And I want to see life,Driving in your car
+  Oh, please don't drop me home
+  Because it's not my home, it's their home
+  And I'm welcome no more
+  And if a double-decker bus
+  Crashes into us
+  To die by your side
+  Is such a heavenly way to die
+  And if a ten ton truck
+  Kills the both of us
+  To die by your side
+  Well, the pleasure, the privilege is mine</p>
+        </div>
+        
+      
+      
+      }
+     
+ 
+      
     </div>
   );
 }
